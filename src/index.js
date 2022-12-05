a0d9ee0fdc8224afd82d413b9fefd868ad0ff65c
María Fernanda Choreño Terán <mfernanda.cht@gmail.com>
2022-12-05 09:27:13 -0600

    tercer commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const botonParticipar = document.getElementById("participar");
 const preguntarNumero = document.getElementById("preguntarNumero");
 const cardNumber = document.getElementById("cardNumber");
 const botonDonar = document.getElementById("donar");
-
+let acumulador = ""
 
 botonParticipar.onclick = function () {
     alert("Hola " + nombre.value + " ¡Gracias por interesarte en ser un Rey Mago!")
@@ -13,9 +13,21 @@ botonParticipar.onclick = function () {
 
 botonDonar.onclick = function () {
     console.log(validator.isvalid(cardNumber.value))
+    console.log(validator.maskify(cardNumber.value))
+    console.log(validator.maskify(acumulador))
+    // para generar mensaje con el acumulador
+    if ((validator.isvalid(cardNumber.value)) === true){
+      alert("Tu tarjeta de crédito fue valida," + acumulador + "muchas gracias por tu donación") 
+    }
+    else {
+        alert("Tarjeta de crédito invalida, por favor introducela nuevamente") 
+    }
 };
 
-// validator.isValid() 
+    
+       
+
+// validator.isValid() ya los llame 
 // validator.maskify()
 
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -3,64 +3,43 @@ const validator = {
   isvalid : (cardNumber) =>  {
    // vamos a ir guardando la suma total en la siguiente variable
     let luhnSum = 0
-    console.log(cardNumber)
     //convertir el string a un array y dividir por espacio
      let numeros = Array.from(cardNumber,Number)
-     console.log(numeros)
       // convierte string a numeros pero sin dividir Array.from(String(12345), Number);
     //funcion que recorre mi array
     for (let i=1; i< numeros.length; i+=2){
       // primer paso doblar el valor de los digitos pares empezando por el segundo digito de la derecha
       let doubleNumber = numeros[i] * 2
-      // si numero es amyor a 10, restar 9, -=9
+      // si numero es mayor a 10, restar 9, -=9
       if (doubleNumber > 9){
         doubleNumber -= 9
       }
       // sumar numeros con doubleNumber = luhnSum
-      
       luhnSum += doubleNumber 
     }
     for (let i=0; i< numeros.length; i+=2){
       // primer paso doblar el valor de los digitos pares empezando por el segundo digito de la derecha
       let singleNumber = numeros[i] 
-
       luhnSum += singleNumber
-      console.log(luhnSum)
     }
-
     console.log(luhnSum)
-    return luhnSum % 10 === 0 
-    
-    /*
-       // para generar mensaje
-    let mensaje = ""
-    if (luhnSum(cardNumber.value)){
-      message =  "Tu tarjeta de crédito fue valida, muchas gracias por tu donación"
+    return luhnSum % 10 === 0    
+}
+// proceso enmascarar
+, maskify: (cardNumber) => {
+  let acumulador= ""
+  let numeros = Array.from(cardNumber,Number)
+  for (let i=0; i< numeros.length; i++){
+    if (i<4){
+      acumulador = acumulador + numeros[i]
     }
     else {
-      message = "Tarjeta de crédito invalida, por favor introducela nuevamente"
+      acumulador = acumulador + "#"
     }
-    */
- 
-}
-
-}
-
-/* proceso enmascarar
-const validator = {
-  maskify : (cardNumber) => {
-    cardNumber = cardNumber.toString() // segun yo ya es string me puedo saltar este paso?
-    acumulador=""
-    for (let i=0; i< cardNumber.length; i++){
-      if (i<4){
-        acumulador = acumulador + cardNumber[i]
-      }
-      else {
-        acumulador = acumulador + "#"
-      }
-    }
-  return acumulador
   }
+  console.log(acumulador)
+return acumulador
 }
-*/
+
+}
 export default validator;
